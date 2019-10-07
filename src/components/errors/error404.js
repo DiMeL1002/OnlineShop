ca2d922a0d51b44bb0118e67d8502994b63f090b
DiMeL1002 <rexxar10@mail.ru>
2019-10-07 16:52:50 +0300

    Верстка
    Добавлена страница 404

---
+++ src/components/errors/error404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './error404.scss'
+
+export default class extends React.Component {
+    render() {
+        return (
+            <div className="error404">
+                <span className="error404__code">404</span>
+                <h1 className="error404__title">Страница не найдена</h1>
+                <p>По ссылке, которую вы пытались открыть, страницы не оказалось. Введите правильный адрес страницы.</p>
+            </div>
+        )
+    }
+}
--- src/pages/error404/error404.js
+++ src/pages/error404/error404.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import Error404 from '~c/errors/error404'
+import PageContainer from '~p/pageContainer/pageContainer'
+
 export default class extends React.Component {
     render() {
         return (
-            <div>Ошибка 404</div>
+            <PageContainer>
+                <section className="error404-page"><Error404 /></section>
+            </PageContainer>
         )
     }
 }
